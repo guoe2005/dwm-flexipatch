bd91a35a0829632c4a0aab1cd5b7122c7c70a505
guoe2005 <guoe2005@126.com>
2024-09-20 20:56:05 +0800

    a

--- config.def.h
+++ config.def.h
@@ -412,7 +412,7 @@ static const Launcher launchers[] = {
 
 #if COOL_AUTOSTART_PATCH
 static const char *const autostart[] = {
-	"st", NULL,
+	"kgx", NULL,
 	NULL /* terminate */
 };
 #endif // COOL_AUTOSTART_PATCH
